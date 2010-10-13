1bc977778fb0baa3bae1d03808663f152fa9418c
David Miers <dave.miers@jax.org>
2010-10-13 13:43:14 +0000

    Fixed to stop duplicate "No Files Queued" messages.

--- loadQueueReport.py
+++ loadQueueReport.py
@@ -43,14 +43,12 @@
 #
 ###########################################################################
 
+import os
 import sys 
 import db
 import string
-import os
+import mgi_utils
 
-
-print "This report provides a list of files that are currently waiting to be"
-print "processed by each job stream."
 
 #
 # Connect to the database.
@@ -76,42 +74,85 @@ cmd.append(
           'fp._JobStream_key = js._JobStream_key')
 
 #
+# Get a list of all job stream names that should appear in the report.
+#
+cmd.append(
+    'select distinct jobStreamName ' + \
+    'from #FileTypes ' + \
+    'order by jobStreamName')
+
+#
 # Get all the files that are waiting to be process by each job stream.
-# The outer join will make sure each job stream is listed, even if there
-# are no files waiting.
 #
 cmd.append(
     'select ft.jobStreamName, fm.fileType, fm.fileName, fileSize ' + \
     'from #FileTypes ft, APP_FilesMirrored fm ' + \
-    'where ft.fileType *= fm.fileType and ' + \
+    'where ft.fileType = fm.fileType and ' + \
           'not exists (select 1 ' + \
                       'from APP_FilesProcessed fp ' + \
                       'where fp._File_key = fm._File_key) ' + \
-    'order by ft.jobStreamName, fm.fileName')
+    'order by ft.jobStreamName, fm.fileType, fm.fileName')
 
 results = db.sql(cmd, 'auto')
 
-lastJobStream = ""
-for r in results[1]:
+#
+# Create a dictionary of all of the files that need to be processed.
+# Each key is the job stream that will process the files and the value is
+# a list of tuples. Each tuple contains the name, type and size of a file
+# that needs to be processed by the corresponding job stream.
+#
+fileDict = {}
+for r in results[2]:
     jobStream = r['jobStreamName']
     fileType = r['fileType']
     fileName = r['fileName']
     fileSize = r['fileSize']
 
     #
-    # Print the job stream name if this is the first record for a new
-    # job stream.
+    # If there is already a list of files for this job stream, get the list
+    # so a new file can be added to it. Otherwise, start a new list.
     #
-    if jobStream != lastJobStream:
-        print "\n\nJob Stream: " + jobStream + "\n"
-        lastJobStream = jobStream
+    if fileDict.has_key(jobStream):
+        list = fileDict[jobStream]
+    else:
+        list = []
 
     #
-    # Print the file name, type and size if they exist.  Otherwise, indicate
-    # that there are no files queued for the current job stream.
+    # Append the new file to the list and update the dictionary.
     #
-    if fileName != None:
-        print fileName + " (" + fileType + ") (" + str(fileSize) + ")"
+    list.append((fileType,fileName,fileSize))
+    fileDict[jobStream] = list
+
+#
+# Print a heading for the report.
+#
+print "This report provides a list of files that are currently waiting"
+print "to be processed by each job stream. The files in this report have"
+print "been logged in radar..APP_FilesMirrored, but have not been logged"
+print "in radar..APP_FilesProcessed."
+print "\nDate: " + mgi_utils.date()
+print "-"*80
+
+#
+# Create a new section in the report for each job stream.
+#
+for r in results[1]:
+    jobStream = r['jobStreamName']
+
+    #
+    # Print the name of the current job stream.
+    #
+    print "\n\nJob Stream: " + jobStream + "\n"
+
+    #
+    # If there is at least one file in the dictionary for this job stream,
+    # get each tuple of file information from the list and print an entry
+    # in the report. Otherwise, indicate that no files are in the queue
+    # for this job stream.
+    #
+    if fileDict.has_key(jobStream):
+        for (fileType,fileName,fileSize) in fileDict[jobStream]:
+            print fileName + " (" + fileType + ") (" + str(fileSize) + ")"
     else:
         print "****  No Files Queued  ****"
 
